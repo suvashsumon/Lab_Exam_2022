4fcb353737e4fdbc8fc6a2812ce574c1e310fae3
suvashkumar <suvashkumar.naogaon@gmail.com>
2022-04-18 11:11:19 +0600

    remote database connected

--- app.js
+++ app.js
@@ -11,8 +11,9 @@ const app = express();
 const PORT = 3000;
 
 // databse connection
-const url = "mongodb://127.0.0.1:27017/labexam";
-const conn = mongoose.connect(url);
+const localDatabaseUrl = "mongodb://127.0.0.1:27017/labexam";
+const remoteDatabaseUrl = "mongodb+srv://suvashkumar:VoR65e2i86fYalCR@cluster0.dzm1b.mongodb.net/labexam?retryWrites=true&w=majority";
+const conn = mongoose.connect(remoteDatabaseUrl);
 conn.then(()=>{
     console.log("Database server connected....");
 }, (err)=>{
