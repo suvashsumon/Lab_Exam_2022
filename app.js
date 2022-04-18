dba22a92848a170b1957cf5b223cebafd6b1057a
suvashkumar <suvashkumar.naogaon@gmail.com>
2022-04-18 10:20:50 +0600

    dogsRouter is created

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const dogsRouter = require("./routes/dogs.router");
 
 const app = express();
 
 const PORT = 3000;
+
+app.use("/api/dogs", dogsRouter);
 
 app.listen(PORT, (req, res)=>{
     console.log("Server is running....");
---
+++ routes/dogs.router.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const { process_params } = require("express/lib/router");
+
+const dogsRouter = express.Router();
+
+dogsRouter.get("/", (req, res)=>{
+    res.send("Showing all the dogs");
+});
+
+dogsRouter.put("/", (req, res)=>{
+    res.send("PUT does not supports by this route.....");
+});
+
+
+dogsRouter.post("/", (req, res)=>{
+    res.send("New dogs is added.");
+});
+
+
+dogsRouter.delete("/", (req, res)=>{
+    res.send("All the dogs are deleted.");
+});
+
+dogsRouter.get("/:id", (req, res)=>{
+    res.send("Showing dog id/name : "+req.params.id);
+});
+
+
+dogsRouter.put("/:id", (req, res)=>{
+    res.send("Updating dog id/name : "+req.params.id);
+});
+
+
+dogsRouter.post("/:id", (req, res)=>{
+    res.send("POST does not supported by this route.");
+});
+
+
+dogsRouter.delete("/:id", (req, res)=>{
+    res.send("Deleted dogs id : "+req.params.id);
+});
+
+
+
+module.exports = dogsRouter;
